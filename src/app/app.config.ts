72f454ee084985fac6e11fb305cc5eb241ff7f4a
Asael <makintosh_ragga@yahoo.es>
2024-12-11 12:35:28 -0500

    feat: deploy ssr version

---
+++ src/app/app.config.ts
@@ -0,0 +1,8 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideClientHydration()
+    ]
+};

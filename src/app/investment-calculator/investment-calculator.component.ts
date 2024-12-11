8120ad84a8124c84060f68a6719c3e2f30d1b82a
Asael <makintosh_ragga@yahoo.es>
2024-12-10 21:47:47 -0500

    refactor: <investment-results />

--- src/app/investment-calculator/investment-calculator.component.ts
+++ src/app/investment-calculator/investment-calculator.component.ts
@@ -50,4 +50,5 @@ export class InvestmentCalculatorComponent {
     }
     return annualData;
   }
+
 }
--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,12 +1,11 @@
 import { Component, input } from '@angular/core';
-import { InvestmentRowComponent } from './investment-row/investment-row.component';
 import { AnnualData } from '../types/annual-data.model';
 import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'investment-results',
   standalone: true,
-  imports: [InvestmentRowComponent, CurrencyPipe],
+  imports: [ CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })

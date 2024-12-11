89c3ed1a1fe860adc0dfc3c0ab376286b7d01485
Asael <makintosh_ragga@yahoo.es>
2024-12-10 22:31:59 -0500

    refactor: lift up investment-header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InvestmentCalculatorComponent } from './investment-calculator/investment-calculator.component';
+import { InvestmentHeaderComponent } from "./investment-header/investment-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [InvestmentCalculatorComponent]
+  imports: [InvestmentCalculatorComponent, InvestmentHeaderComponent]
 })
 export class AppComponent { }
--- src/app/investment-calculator/investment-calculator.component.ts
+++ src/app/investment-calculator/investment-calculator.component.ts
@@ -3,12 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { Investment } from '../types/investment.model';
 import { AnnualData } from '../types/annual-data.model';
 import { InvestmentResultsComponent } from "../investment-results/investment-results.component";
-import { InvestmentHeaderComponent } from '../investment-header/investment-header.component';
 
 @Component({
   selector: 'investment-calculator',
   standalone: true,
-  imports: [FormsModule, InvestmentHeaderComponent, InvestmentResultsComponent],
+  imports: [FormsModule, InvestmentResultsComponent],
   templateUrl: './investment-calculator.component.html',
   styleUrl: './investment-calculator.component.css'
 })
